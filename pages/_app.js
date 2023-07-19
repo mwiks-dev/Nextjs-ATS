b754abb196f5ea55a35a7849aa91c6410b81ea70
Maryann Mwikali <mwikali119@gmail.com>
2023-07-19 20:46:50 +0300

    Add bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.css'
 import SignUp from "./signUpForm";
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <SignUp/>
